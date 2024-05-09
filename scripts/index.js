64e755529f3868f30fd6219ffa56008b3ab35550
Yauheni Paulovich <e.pavlovich29@gmail.com>
2024-05-09 14:24:55 +0300

    refactor: change behavior dark theme

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,23 @@
-const chk = document.querySelector('#chk');
-console.log(chk);
-chk.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
+const chk = document.querySelector("#chk");
+let mail = document.getElementById("icon_mail");
+let geo = document.getElementById("icon_geo");
+
+chk.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (mail.src.endsWith("mail.png")) {
+    // Если отображается первое изображение, заменяем его на второе
+    mail.src = "./images/icon/mail_white.png";
+  } else {
+    // Иначе, если отображается второе изображение, заменяем его на первое
+    mail.src = "./images/icon/mail.png";
+  }
+
+  if (geo.src.endsWith("geo.png")) {
+    // Если отображается первое изображение, заменяем его на второе
+    geo.src = "./images/icon/geo_white.png";
+  } else {
+    // Иначе, если отображается второе изображение, заменяем его на первое
+    geo.src = "./images/icon/geo.png";
+  }
 });
